9a55bdb201b2d00a7e10a33f494d5d4e9f835764
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2022-06-27 16:55:03 +0100

    Implementação do formulário de LogIn

--- src/Routes/LogIn/login-component.jsx
+++ src/Routes/LogIn/login-component.jsx
@@ -1,7 +1,21 @@
+import './login-component.scss';
+
 function LogIn(){
     return(
         <div>
+            <form method='get' action="">
+                <input type="text" name='e-mail' aria-label='E-mail' placeholder='Coloque o seu e-mail' maxLength='30' required />
 
+                <label htmlFor="loginPassword"></label>
+                <input type="password" name='loginPassword' placeholder='Coloque a sua password' minLength='3' maxLength='30' required />
+
+                <div>
+                    <label htmlFor="loginCheckBox">
+                        <input type="checkbox" name='loginCheckBox' aria-label='loginCheckBox' />Guardar os meus dados
+                    </label>
+                </div>
+                <button type='submit'>Entrar</button>
+            </form>
         </div>
     )
 }
