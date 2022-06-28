629a2baecd5652e2475f9cfab28afddfb5c41da9
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2022-06-28 16:04:57 +0100

    Implemntação de estilização do formulário de LogIn

--- src/Routes/LogIn/login-component.jsx
+++ src/Routes/LogIn/login-component.jsx
@@ -2,19 +2,24 @@ import './login-component.scss';
 
 function LogIn(){
     return(
-        <div>
-            <form method='get' action="">
-                <input type="text" name='e-mail' aria-label='E-mail' placeholder='Coloque o seu e-mail' maxLength='30' required />
-
-                <label htmlFor="loginPassword"></label>
-                <input type="password" name='loginPassword' placeholder='Coloque a sua password' minLength='3' maxLength='30' required />
-
-                <div>
+        <div >
+            <form className='text-center registerForm' method='get' action="">
+                <div className='form-group pt-3'>
+                    <label htmlFor="loginEmail"></label>
+                    <input className='border-info inputDetailconfiguration' type="text" name='e-mail' aria-label='E-mail' placeholder='Coloque o seu e-mail' maxLength='30' required />
+                </div>
+                <div className='form-group pt-4 pb-2'>
+                    <label htmlFor="loginPassword"></label>
+                    <input className='border-info inputDetailconfiguration' type="password" name='loginPassword' placeholder='Coloque a sua password' minLength='3' maxLength='30' required />
+                </div>
+                <div className='form-group pt-3'>
                     <label htmlFor="loginCheckBox">
                         <input type="checkbox" name='loginCheckBox' aria-label='loginCheckBox' />Guardar os meus dados
                     </label>
                 </div>
-                <button type='submit'>Entrar</button>
+                <div className='pt-3'>
+                    <button className='btn btn-secondary' type='submit'>Entrar</button>
+                </div>
             </form>
         </div>
     )
